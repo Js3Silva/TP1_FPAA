7b45911a420010091c34afe8535962784e27945a
Pedro <pedroluizns@hotmail.com>
2026-04-21 17:44:22 -0300

    refact and fix kruskal

--- TP1/src/kruskal/Kruskal.java
+++ TP1/src/kruskal/Kruskal.java
@@ -1,6 +1,10 @@
 package kruskal;
 
 import dsu.Dsu;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 import java.lang.reflect.Constructor;
@@ -8,26 +12,33 @@ import java.lang.reflect.Constructor;
 public class Kruskal {
     private static final Map<Class<?>, Constructor<? extends Dsu>> constructorCache = new HashMap<>();
 
-    public static Dsu executarKruskalDeVarianteParaGrafo(Class<? extends Dsu> clazz, Grafo grafo) {
+    public static void executarKruskalDeVarianteParaGrafo(Class<? extends Dsu> clazz, Grafo grafo) {
         Dsu variante;
         var construtorClazz = getConstructor(clazz);
         try {
             variante = construtorClazz.newInstance(grafo.getNumVertices());
         } catch (Exception e) {
             System.out.println("Incapaz de instanciar variante de DSU do tipo:" + clazz.getName());
-            return null;
+            return;
         }
 
         int mstAlvo = grafo.getNumVertices() - 1;
         int mstCount = 0;
-        for (Aresta aresta : grafo.getArestas()) {
+
+        var arestas = ordenaArestasPorPeso(grafo.getArestas());
+        for (Aresta aresta : arestas) {
             if (mstCount == mstAlvo) break;
             if (variante.find(aresta.origem) != variante.find(aresta.destino)) {
                 variante.union(aresta.origem, aresta.destino);
                 mstCount++;
             }
         }
-        return variante;
+    }
+
+    private static List<Aresta> ordenaArestasPorPeso(List<Aresta> arestas) {
+        List<Aresta> sorted = new ArrayList<>(arestas);
+        sorted.sort(Comparator.comparingInt(a -> a.peso));
+        return sorted;
     }
 
     @SuppressWarnings("unchecked")
@@ -43,4 +54,5 @@ public class Kruskal {
             }
         });
     }
+
 }
